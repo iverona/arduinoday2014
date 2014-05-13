4326a545b7005223e52e9999f4752805b410936a
Ignacio Verona <ignacio.verona@blue-tc.com>
2014-05-13 20:38:04 +0200

    Cleaning

--- Theremin/S4Afirmware14_distancia/S4Afirmware14_distancia.ino
+++ Theremin/S4Afirmware14_distancia/S4Afirmware14_distancia.ino
@@ -1,3 +1,6 @@
+// NEW IN VERSION 1.5:
+// Changed pin 8 from standard servo to normal digital output
+
 // NEW IN VERSION 1.4:
 // Changed Serial.print() for Serial.write() in ScratchBoardSensorReport function to make it compatible with latest Arduino IDE (1.0)
 
@@ -19,94 +22,91 @@ int states[10];
 unsigned long initialPulseTime;
 unsigned long lastDataReceivedTime;
 
-float distancia;
-unsigned long pulso; 
-
 volatile boolean updateServoMotors;
 volatile boolean newInterruption;
 
+float distancia;
+unsigned long pulso;
+
 void setup()
 {
- distancia = 0;
- Serial.begin(38400);
- Serial.flush();
- configurePins();
- configureServomotors();
- lastDataReceivedTime = millis();
+  Serial.begin(38400);
+  Serial.flush();
+  configurePins();
+  configureServomotors();
+  lastDataReceivedTime = millis();
 }
 
 void loop()
 {
- if (updateServoMotors)
- {
-   sendUpdateServomotors();
-   sendSensorValues();
-   updateServoMotors = false;
- }
- else
- {
-   readSerialPort();
- }
+  if (updateServoMotors)
+  {
+    sendUpdateServomotors();
+    sendSensorValues();
+    updateServoMotors = false;
+  }
+  else
+  {
+    readSerialPort();
+  }
 }
 
 void configurePins()
 {
- for (int index = 0; index < 10; index++)
- {
+  for (int index = 0; index < 10; index++)
+  {
     states[index] = 0;
     pinMode(index+4, OUTPUT);
     digitalWrite(index+4, LOW); //reset pins
- }
+  }
 
- outputs[1] = 'a';//pin 5
- outputs[2] = 'a';//pin 6
- outputs[5] = 'a';//pin 9
- outputs[6] = 'X';//pin 10
- outputs[7] = 'd';//pin 11
- outputs[9] = 'd';//pin 13
+  pinMode(2,INPUT);
+  pinMode(3,INPUT);
 
- pinMode(2,INPUT);
- pinMode(3,INPUT);
- outputs[0] = 'c'; //pin 4
- outputs[3] = 'c'; //pin 7
- outputs[4] = 's'; //pin 8
- outputs[8] = 's'; //pin 12
+  outputs[0] = 'c'; //pin 4
+  outputs[1] = 'a'; //pin 5
+  outputs[2] = 'a'; //pin 6
+  outputs[3] = 'c'; //pin 7
+  outputs[4] = 's'; //pin 8
+  outputs[5] = 'a'; //pin 9
+  outputs[6] = 'X'; //pin 10
+  outputs[7] = 'd'; //pin 11
+  outputs[8] = 'd'; //pin 12
+  outputs[9] = 'd'; //pin 13
 }
 
 void configureServomotors() //servomotors interruption configuration (interruption each 10 ms on timer2)
 {
- newInterruption = false;
- updateServoMotors = false;
+  newInterruption = false;
+  updateServoMotors = false;
 
- TCCR2A = 0;
- TCCR2B = 1<<CS22 | 1<<CS21 | 1<<CS20;
- TIMSK2 = 1<<TOIE2; //timer2 Overflow Interrupt
- TCNT2 = TIMER2_PRELOAD; //start timer
+  TCCR2A = 0;
+  TCCR2B = 1<<CS22 | 1<<CS21 | 1<<CS20;
+  TIMSK2 = 1<<TOIE2; //timer2 Overflow Interrupt
+  TCNT2 = TIMER2_PRELOAD; //start timer
 }
 
 void sendSensorValues()
 {
   int sensorValues[6], readings[5], sensorIndex;
-    // original  for (sensorIndex = 0; sensorIndex < 6; sensorIndex++) cambiada sensorIndex < 6 por sensorIndex < 5 el sensor 6 tiene el valor de distancia
-    for (sensorIndex = 0; sensorIndex < 5; sensorIndex++) //for analog sensors, calculate the median of 5 sensor readings in order to avoid variability and power surges
-    {
-      for (int p = 0; p < 5; p++)
-        readings[p] = analogRead(sensorIndex);
-      InsertionSort(readings, 5); //sort readings
-      sensorValues[sensorIndex] = readings[2]; //select median reading
-    }
+  for (sensorIndex = 0; sensorIndex < 5; sensorIndex++) //for analog sensors, calculate the median of 5 sensor readings in order to avoid variability and power surges
+  {
+    for (int p = 0; p < 5; p++)
+      readings[p] = analogRead(sensorIndex);
+    InsertionSort(readings, 5); //sort readings
+    sensorValues[sensorIndex] = readings[2]; //select median reading
+  }
 
-    //send analog sensor values
-    // original  for (sensorIndex = 0; sensorIndex < 6; sensorIndex++) cambiada sensorIndex < 6 por sensorIndex < 5 el sensor 6 tiene el valor de distancia
-    for (sensorIndex = 0; sensorIndex < 5; sensorIndex++)
-      ScratchBoardSensorReport(sensorIndex, sensorValues[sensorIndex]);
-    
-    //send distancia value 
-    ScratchBoardSensorReport(5, distancia);
+  //send analog sensor values
+  for (sensorIndex = 0; sensorIndex < 5; sensorIndex++)
+    ScratchBoardSensorReport(sensorIndex, sensorValues[sensorIndex]);
 
-    //send digital sensor values
-    ScratchBoardSensorReport(6, digitalRead(2)?1023:0);
-    ScratchBoardSensorReport(7, digitalRead(3)?1023:0);
+  //Enviar distancia
+  ScratchBoardSensorReport(5, distancia);
+
+  //send digital sensor values
+  ScratchBoardSensorReport(6, digitalRead(2)?1023:0);
+  ScratchBoardSensorReport(7, digitalRead(3)?1023:0);
 }
 
 void InsertionSort(int* array, int n)
@@ -126,8 +126,8 @@ void swap (int* array, int a, int b)
 void ScratchBoardSensorReport(int sensor, int value) //PicoBoard protocol, 2 bytes per sensor
 {
   Serial.write( B10000000
-                | ((sensor & B1111)<<3)
-                | ((value>>7) & B111));
+    | ((sensor & B1111)<<3)
+    | ((value>>7) & B111));
   Serial.write( value & B1111111);
 }
 
@@ -149,7 +149,7 @@ void readSerialPort()
       if (inByte <= 127) // This prevents Linux ttyACM driver to fail
       {
         states[pin - 4] = ((sensorHighByte & 0x07) << 7) | (inByte & 0x7F);
-          updateActuator(pin - 4);
+        updateActuator(pin - 4);
       }
     }
   }
@@ -176,18 +176,16 @@ void reset() //with xbee module, we need to simulate the setup execution that oc
 
 void updateActuator(int pinNumber)
 {
-  pinMode(10, OUTPUT);     // ponemos el pin como salida
-  digitalWrite(10, HIGH);  // lo activamos
-  delayMicroseconds(10);    // esperamos 10 microsegundos
-  digitalWrite(10, LOW);   // lo desactivamos
-  pinMode(A5, INPUT);      // cambiamos el pin como entrada
-  pulso = pulseIn(A5, HIGH);
-  distancia = ((float(pulso/1000.0))*34.32)/2;
-  if (outputs[pinNumber] == 'd'){
-     digitalWrite(pinNumber + 4, states[pinNumber]);
-  }else if (outputs[pinNumber] == 'a'){
-     analogWrite(pinNumber + 4, states[pinNumber]);
-  }
+  pinMode(10, OUTPUT); // ponemos el pin 10 como salida
+  digitalWrite(10, HIGH); // lo activamos
+  delayMicroseconds(10); // esperamos 10 microsegundos
+  digitalWrite(10, LOW); // lo desactivamos
+  pinMode(A5, INPUT); // ponemos el pin 11 como entrada
+  pulso = pulseIn(A5, HIGH); //tiempo de llegada del eco
+  distancia = ((float(pulso/1000.0))*34.32)/2; // pasar tiempo a cm.
+  
+  if (outputs[pinNumber] == 'd')  digitalWrite(pinNumber + 4, states[pinNumber]);
+  else if (outputs[pinNumber] == 'a')  analogWrite(pinNumber + 4, states[pinNumber]);
 }
 
 void sendUpdateServomotors()
@@ -207,7 +205,7 @@ void servomotorC (int pinNumber, int dir)
 
 void servomotorS (int pinNumber, int angle)
 {
-  if (angle < 0) pulse(pinNumber, 0);
+  if (angle < 0) pulse(pinNumber, 600);
   else if (angle > 180) pulse(pinNumber, 2400);
   else pulse(pinNumber, (angle * 10) + 600);
 }
@@ -217,7 +215,8 @@ void pulse (int pinNumber, int pulseWidth)
   initialPulseTime = micros();
   digitalWrite(pinNumber, HIGH);
 
-  while (micros() < pulseWidth + initialPulseTime){}
+  while (micros() < pulseWidth + initialPulseTime){
+  }
   digitalWrite(pinNumber, LOW);
 }
 
@@ -238,4 +237,5 @@ ISR(TIMER2_OVF_vect) //timer1 overflow interrupt vector handler
   newInterruption = !newInterruption;
   TCNT2 = TIMER2_PRELOAD;  //reset timer
 }
+
 
